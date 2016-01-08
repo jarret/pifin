486cbfc15ebbb6fce674be636cc1b3bbbe4f6d80
Jarret Dyrbye <jarret.dyrbye@gmail.com>
2016-01-07 20:30:02 -0700

    Collect loose stuff into CurrencyReport class

--- currencyReport.py
+++ currencyReport.py
@@ -15,11 +15,11 @@ DEBUG = False
 ################################################################################
 
 class CurrencyPair(YFCurrency):
-    def __init__(self, base_currency_name, quote_currency_name):
-        super(CurrencyPair, self).__init__("%s%s" % (base_currency_name,
-                                                     quote_currency_name))
-        self.base_currency = base_currency_name
-        self.quote_currency = quote_currency_name
+    def __init__(self, currencyNamePair):
+        super(CurrencyPair, self).__init__("%s%s" % (currencyNamePair['base'],
+                                                     currencyNamePair['quote']))
+        self.base_currency = currencyNamePair['base']
+        self.quote_currency = currencyNamePair['quote']
 
     def _print_data(self, data):
         for label, val in data.items():
@@ -33,6 +33,70 @@ class CurrencyPair(YFCurrency):
 
     def get_id(self):
         return self.data_set['id']
+
+################################################################################
+# report class
+################################################################################
+
+class CurrencyReport(object):
+    def __init__(self, currencyNames):
+        self.currencyNames = currencyNames
+        self.currencyNamePairs = []
+        for base_currency_name in self.currencyNames:
+            for quote_currency_name in self.currencyNames:
+                if base_currency_name != quote_currency_name:
+                    currencyNamePair = {}
+                    currencyNamePair['base'] = base_currency_name
+                    currencyNamePair['quote'] = quote_currency_name
+                    self.currencyNamePairs.append(currencyNamePair)
+    def fetch(self):
+        #use a process pool to send the queries in parallel
+        pool = Pool(processes=len(self.currencyNamePairs))
+        self.currencyPairs = pool.map(CurrencyPair, self.currencyNamePairs)
+
+    def _get_currency_pair_rate(self, base_currency_name, quote_currency_name):
+        for pair in self.currencyPairs:
+            if pair.base_currency == base_currency_name and \
+               pair.quote_currency == quote_currency_name:
+                return pair.get_rate()
+        return None
+
+    def _rate_to_str(self, rate):
+        return "%11.4f" % float(rate)
+
+    def __str__(self):
+        def horizontal_bar():
+            bar = "---------"
+            for currency in self.currencyNames:
+                bar = bar + "-----------"
+            return bar + "--\n"
+
+        def base_line(base_currency_name):
+            line = "| 1 %s |" % base_currency_name
+            for quote_currency_name in self.currencyNames:
+                if quote_currency_name == base_currency_name:
+                    rate = 1.0
+                else:
+                    rate = self._get_currency_pair_rate(base_currency_name,
+                                                        quote_currency_name)
+                line = line + "%s" % self._rate_to_str(rate)
+            return line + " |\n"
+
+        def title_line():
+            titleBar = "|       |"
+            for currency in self.currencyNames:
+                titleBar = titleBar + "%11s" % currency
+            return titleBar + " |\n"
+
+        string = ""
+        string = string + "fetched at: %s\n" % datetime.datetime.now()
+        string = string + horizontal_bar()
+        string = string + title_line()
+        string = string + horizontal_bar()
+        for base_currency_name in self.currencyNames:
+            string =  string + base_line(base_currency_name)
+        string = string + horizontal_bar()
+        return string
 
 ################################################################################
 # deal with input currency file
@@ -55,87 +119,6 @@ for line in open(currencyFile).readlines():
     currencyName = line.rstrip()
     currencyNames.append(currencyName)
 
-################################################################################
-# figure out which currency pairs we need to look up
-################################################################################
-
-currencyNamePairs = []
-for base_currency_name in currencyNames:
-    for quote_currency_name in currencyNames:
-        if base_currency_name != quote_currency_name:
-            currencyNamePair = {}
-            currencyNamePair['base'] = base_currency_name
-            currencyNamePair['quote'] = quote_currency_name
-            currencyNamePairs.append(currencyNamePair)
-
-################################################################################
-# fetch data for currency pairs
-################################################################################
-
-def make_currency_pair_instance(currencyNamePair):
-    if DEBUG:
-        print "querying %s %s" % (currencyNamePair['base'],
-                                  currencyNamePair['quote'])
-    currencyPair = CurrencyPair(currencyNamePair['base'],
-                                currencyNamePair['quote'])
-    if DEBUG:
-        print "got %s %s" % (currencyNamePair['base'],
-                             currencyNamePair['quote'])
-    return currencyPair
-
-#use a process pool to send the queries in parallel
-pool = Pool(processes=len(currencyNamePairs))
-currencyPairs = pool.map(make_currency_pair_instance, currencyNamePairs)
-
-################################################################################
-# get rate from currency pair instance
-################################################################################
-
-def get_currency_pair_rate(base_currency_name, quote_currency_name):
-    for pair in currencyPairs:
-        if pair.base_currency == base_currency_name and \
-           pair.quote_currency == quote_currency_name:
-            return pair.get_rate()
-    return None
-
-################################################################################
-# format particular strings
-################################################################################
-
-def rate_to_str(rate):
-    return "%11.4f" % float(rate)
-
-################################################################################
-# generate report
-################################################################################
-
-def print_horizontal_bar():
-    bar = "---------"
-    for currency in currencyNames:
-        bar = bar + "-----------"
-    print bar + "--"
-
-def print_base_line(base_currency_name):
-    line = "| 1 %s |" % base_currency_name
-    for quote_currency_name in currencyNames:
-        if quote_currency_name == base_currency_name:
-            rate = 1.0
-        else:
-            rate = get_currency_pair_rate(base_currency_name,
-                                          quote_currency_name)
-        line = line + "%s" % rate_to_str(rate)
-    print line + " |"
-
-def print_title_line():
-    titleBar = "|       |"
-    for currency in currencyNames:
-        titleBar = titleBar + "%11s" % currency
-    print titleBar + " |"
-
-print "fetched at: %s" % datetime.datetime.now()
-print_horizontal_bar()
-print_title_line()
-print_horizontal_bar()
-for base_currency_name in currencyNames:
-    print_base_line(base_currency_name)
-print_horizontal_bar()
+report = CurrencyReport(currencyNames)
+report.fetch()
+print report
